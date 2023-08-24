0d16c070e4e942abf16ecb9f8347150f2354aeaa
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2023-08-23 21:27:30 -0400

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className="title-container">
+          <div className="title">
+            <h2>Tiempo</h2>
+          </div>
+        </div>
+      </header>
+      <section className=""></section>
+    </>
+  );
+}
+
+export default App;
