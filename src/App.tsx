7a5b3bc70a4288e5ee569e299be93270402ada9f
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2023-08-26 00:38:39 -0400

    resonsive design

--- src/App.tsx
+++ src/App.tsx
@@ -4,51 +4,101 @@ import { Cloud, Foog, Rainy, Snowy, Storm } from "./assets";
 function App() {
   return (
     <>
-      <section className="h-full">
+      <section className="h-full flex flex-wrap">
         <header className="h-1/6 w-full flex flex-col justify-center items-center">
           <div className="w-1/2">
-            <div className="w-full bg-green-400">
+            <div className="w-full p-2 bg-cyan-500 rounded-md shadow-md">
               <h1>Tiempo</h1>
             </div>
           </div>
         </header>
-        <div className="grid grid-cols-5 h-5/6 px-4">
-          <div className="px-4">
-            <div className="bg-green-400">
-              <img src={Cloud}></img>
+        <div className="relative h-5/6 w-full overflow-auto">
+          <div
+            className="grid
+            h-[150%]
+            px-4
+            grid-cols-2
+            grid-rows-3
+            gap-4 
+            absolute 
+            w-full
+            grid-flow-row
+            sm:h-full
+            lg:grid-cols-5 
+            lg:grid-rows-1 
+            sm:grid-cols-2 
+            sm:grid-rows-3"
+          >
+            <div className="grid grid-rows-3 overflow-hidden sm:h-auto">
+              <div className="bg-cyan-500 rounded-md shadow-md p-2">
+                <img
+                  className="h-full w-full object-scale-down"
+                  src={Cloud}
+                ></img>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>Lunes</p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>31°</p>
+              </div>
             </div>
-            <div>
-              <p>Lunes</p>
-              <p>31°</p>
+            <div className="grid grid-rows-3 overflow-hidden sm:h-auto">
+              <div className="bg-cyan-500 rounded-md shadow-md p-2">
+                <img
+                  className="h-full w-full object-scale-down"
+                  src={Foog}
+                ></img>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>Martes</p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>31°</p>
+              </div>
             </div>
-          </div>
-          <div className="px-4">
-            <div className="bg-green-400">
-              <img src={Foog}></img>
+            <div className="grid grid-rows-3 overflow-hidden sm:h-auto">
+              <div className="bg-cyan-500 rounded-md shadow-md p-2">
+                <img
+                  className="h-full w-full object-scale-down"
+                  src={Rainy}
+                ></img>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>Miercoles</p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>31°</p>
+              </div>
             </div>
-            <p>Martes</p>
-            <p>31°</p>
-          </div>
-          <div className="px-4">
-            <div className="bg-green-400">
-              <img src={Rainy}></img>
+            <div className="grid grid-rows-3 overflow-hidden sm:h-auto">
+              <div className="bg-cyan-500 rounded-md shadow-md p-2">
+                <img
+                  className="h-full w-full object-scale-down"
+                  src={Snowy}
+                ></img>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>Jueves</p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>31°</p>
+              </div>
             </div>
-            <p>Miercoles</p>
-            <p>31°</p>
-          </div>
-          <div className="px-4">
-            <div className="bg-green-400">
-              <img src={Snowy}></img>
+            <div className="grid col-span-2 lg:col-span-1 grid-rows-3 overflow-hidden sm:h-auto">
+              <div className="bg-cyan-500 rounded-md shadow-md p-2">
+                <img
+                  className="h-full w-full object-scale-down"
+                  src={Storm}
+                ></img>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>Viernes</p>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <p>31°</p>
+              </div>
             </div>
-            <p>Jueves</p>
-            <p>31°</p>
-          </div>
-          <div className="px-4">
-            <div className="bg-green-400">
-              <img src={Storm}></img>
-            </div>
-            <p>Viernes</p>
-            <p>31°</p>
           </div>
         </div>
       </section>
